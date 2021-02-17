d22812ab234ee17b5f9bfcd77355aa0fd7e0ec55
vsvietlichnyi <vladyslav.svietlichnyi@leongaming.com>
2021-02-17 08:27:17 +0200

    made pageObjects

---
+++ src/test/java/pageObjects.java
@@ -0,0 +1,20 @@
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class pageObjects {
+    public final SelenideElement cookieBtnOne = $("button.ui-cookie-consent__manage-button");
+    public final SelenideElement cookieBtnTwo = $("button.button.-guiding.-left.ui-cookie-consent__save-choice-button");
+    public final SelenideElement calculatorContainer = $("button.button.-guiding.-left.ui-cookie-consent__save-choice-button");
+    public final SelenideElement applyingWithCoApplicantCheckbox = $("button.button.-guiding.-left.ui-cookie-consent__save-choice-button");
+    public final SelenideElement moreThenOneDependantCheckbox = $x("//label[@for='dependantsCheck'][1]");
+    public final SelenideElement dependantInTheFamilyRadioBtnTwo = $x("//label[@for='dependants2'][1]");
+    public final SelenideElement existingLoanObligationsCheckbox = $x("//label[@for='obligationsCheck'][1]");
+    public final SelenideElement totalMonthlyIncome = $("#income");
+    public final SelenideElement downPaymentInput = $("#downpayment");
+    public final SelenideElement loanAmountInput = $x("//*[@id='slider1']/div[2]/input");
+    public final SelenideElement loanTermInput = $x("//*[@id='slider2']/div[2]/input");
+    public final SelenideElement monthlyLoanPayment = $("#month-payment");
+    public final SelenideElement maximumLoanAmount = $("#slider-financed");
+}
--- src/test/java/swedbankTestDefinitions.java
+++ src/test/java/swedbankTestDefinitions.java
@@ -1,82 +1,77 @@
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
-import java.util.*;
-
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.Assert.assertEquals;
 
 public class swedbankTestDefinitions {
 
+    pageObjects calculator = new pageObjects();
+
     @Given("Open browser with Swedbank loan calculator")
-    public void openGoogleSearch() {
+    public void openSwedbankLoanCalculatorPage() {
         open("https://www.swedbank.lt/private/credit/loans/home?language=ENG");
-        $(By.cssSelector("button.ui-cookie-consent__manage-button")).click();
-        $(By.cssSelector("button.button.-guiding.-left.ui-cookie-consent__save-choice-button")).click();
-        $(By.cssSelector("#calculator-section")).scrollIntoView(true);
+        calculator.cookieBtnOne.click();
+        calculator.cookieBtnTwo.click();
+        calculator.calculatorContainer.scrollIntoView(true);
     }
 
     @When("I am applying for a loan with a co-applicant -> NO")
     public void iAmApplyingForALoanWithACoApplicantNO() {
-//        $(By.xpath("//label[@for='borrowersCheck'][1]")).click();
+//        calculator.applyingWithCoApplicantCheckbox.click();
     }
 
     @When("I have children \\(dependants) in family -> YES")
     public void iHaveChildrenDependantsInFamilyYES() {
-        $(By.xpath("//label[@for='dependantsCheck'][1]")).click();
+        calculator.moreThenOneDependantCheckbox.click();
     }
 
     @When("Dependants in family -> 2 or more")
     public void dependantsInFamilyOrMore() {
-        $(By.xpath("//label[@for='dependants2'][1]")).click();
-    }
-
-    @When("Total monthly income -> {int} EUR")
-    public void totalMonthlyIncomeEUR(int arg0) {
-        $(By.cssSelector("#income")).click();
-        $(By.cssSelector("#income")).sendKeys(Keys.BACK_SPACE);
-        $(By.cssSelector("#income")).sendKeys(String.valueOf(arg0));
-        Selenide.sleep(50000);
+        calculator.dependantInTheFamilyRadioBtnTwo.click();
     }
 
     @When("I have existing loan obligations \\(incl. in Swedbank) -> NO")
     public void iHaveExistingLoanObligationsInclInSwedbankNO() {
+//        calculator.existingLoanObligationsCheckbox.click();
+    }
+
+    @When("Total monthly income -> {int} EUR")
+    public void totalMonthlyIncomeEUR(int amount) {
+        calculator.totalMonthlyIncome.click();
+        calculator.totalMonthlyIncome.sendKeys(Keys.BACK_SPACE);
+        calculator.totalMonthlyIncome.sendKeys(String.valueOf(amount));
     }
 
     @When("Choose loan amount -> {int} eur")
-    public void chooseLoanAmountEur(String arg0) {
+    public void chooseLoanAmountEur(int amount) {
+        calculator.loanAmountInput.click();
+        calculator.loanAmountInput.sendKeys(Keys.chord(Keys.ARROW_RIGHT, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE));
+        calculator.loanAmountInput.setValue(String.valueOf(amount));
     }
 
     @When("Choose loan term -> {int} years")
-    public void chooseLoanTermYears(int arg0) {
+    public void chooseLoanTermYears(int years) {
+        int months = years * 12;
+        calculator.loanTermInput.click();
+        calculator.loanTermInput.sendKeys(Keys.chord(Keys.ARROW_RIGHT, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE));
+        calculator.loanTermInput.sendKeys(String.valueOf(months));
+        calculator.downPaymentInput.click();
     }
 
     @Then("Find out monthly payment")
     public void findOutMonthlyPayment() {
+        String monthlyPayment = calculator.monthlyLoanPayment.getText();
+        System.out.println("Maximum loan amount = " + monthlyPayment);
+        assertEquals("386", monthlyPayment);
     }
 
     @Then("Find out loan amount")
     public void findOutLoanAmount() {
+        String loanAmount = calculator.maximumLoanAmount.getText();
+        System.out.println("Maximum loan amount = " + loanAmount);
+        assertEquals("49739", loanAmount);
     }
-
-//    @When("I am applying for a loan with a co-applicant -> NO")
-//    public void enterKeyword(String keyword) {
-//        $(By.name("q")).val(keyword).pressEnter();
-//    }
-//
-//    @Then("at least top {int} matches should be shown")
-//    public void topTenMatchesShouldBeShown(int resultsCount) {
-//        $$("#res .g").shouldHave(sizeGreaterThanOrEqual(resultsCount));
-//    }
-//
-//    @Then("the first one should contain {string}")
-//    public void theFirstOneShouldContainKeyword(String expectedText) {
-//        $("#res .g").shouldHave(text(expectedText));
-//    }
 }
